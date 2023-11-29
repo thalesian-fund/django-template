19e19139cda706efe662988a16e4a59e08f775f4
Benjamin Hocking <benjaminhocking25@gmail.com>
2023-11-29 14:25:56 +0000

    add init file

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'my_app.config.MyAppConfig',
+    'my_app.apps.MyAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
